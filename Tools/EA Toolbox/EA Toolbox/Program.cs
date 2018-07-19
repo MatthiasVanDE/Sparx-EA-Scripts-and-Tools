eab7d5e29d779b05b13a618e1725efc3a74b4452
MatthiasVanDE <matthias.vanderelst@gmail.com>
2018-07-19 19:23:56 +0200

    added EA Toolbox

---
+++ Tools/EA Toolbox/EA Toolbox/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vandem11
+ * Date: 19/07/2018
+ * Time: 19:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace EA_Toolbox
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
